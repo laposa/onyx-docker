baa7639859fc568d26f7be928f3b2c16b0318e15
Norbert Laposa <norbert@laposa.ie>
2018-12-17 18:31:30 +0000

    fix for large cache directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ echo "Fixing permissions on var folder"
 chmod a+rwx /srv/*/var/
 
 echo "Delete cache"
-rm -rf /srv/*/var/cache/*
+#rm -rf /srv/*/var/cache/*
+find /srv/*/var/cache/* -maxdepth 1 -delete
 
 /usr/bin/supervisord
